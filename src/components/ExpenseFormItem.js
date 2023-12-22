3c7dae8bbbc8a3eb8fa5c1e9e17907916df7f8be
YongHyunKing <yhkim8246@gmail.com>
2023-12-22 19:39:11 +0900

    feat: add initial component code

--- src/components/ExpenseFormItem.js
+++ src/components/ExpenseFormItem.js
@@ -1,1 +1,11 @@
+import React, { Component } from 'react'
 
+export class ExpenseFormItem extends Component {
+  render() {
+    return (
+      <div>ExpenseFormItem</div>
+    )
+  }
+}
+
+export default ExpenseFormItem
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class ExpenseList extends Component {
+  render() {
+    return (
+      <div>ExpenseList</div>
+    )
+  }
+}
+
+
+export default ExpenseList
