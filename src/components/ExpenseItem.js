ec4e951588eefcf2022a19cea437686f4ca56cb2
YongHyunKing <yhkim8246@gmail.com>
2023-12-22 20:28:28 +0900

    feat: add ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './ExpenseItem.css';
 
 export class ExpenseFormItem extends Component {
   render() {
     return (
-      <div>ExpenseFormItem</div>
+      <li className='item'>
+        <div className='info'>
+          <span className='expense'>학원등록</span>
+          <span className='amount'> 원</span>
+        </div>
+        <div>
+          <button className='edit-btn'>수정</button>
+          <button className='clear-btn'>삭제</button>
+        </div>
+
+      </li>
     )
   }
 }
